d094ac4404593c674df71fd7a29ac1e95918af79
atton <e115763@gmail.com>
2017-07-13 13:10:57 +0900

    Const

---
+++ chapter2/const.php
@@ -0,0 +1,6 @@
+<?php
+const BOOK = 'Perfect PHP'; // namespace scoped
+echo BOOK, PHP_EOL;
+
+?>
+
---
+++ chapter2/define.php
@@ -0,0 +1,7 @@
+<?php
+
+define('BOOK', 'Perfect PHP');  // const
+echo BOOK, PHP_EOL; // scope is global when access not need $
+
+?>
+
