f36dc40b394fa0185e585921ff94c83b08563a89
atton <e115763@gmail.com>
2017-07-13 15:13:00 +0900

    Add integer

---
+++ chapter3/integer.php
@@ -0,0 +1,17 @@
+<?php
+
+$int1 = 1;
+$int2 = -1;
+$int3 = 011;
+$int4 = 0xff;
+$int5 = PHP_INT_MAX;
+$int6 = PHP_INT_MAX + 1; // float
+
+echo $int6;
+
+$int = intval("1");
+$int = (int)"1";
+
+
+?>
+
